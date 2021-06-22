d827ae947db2a7a74331d9b7321de5cee1ac9122
Mohammad Jawad <p176023@nu.edu.pk>
2021-06-22 15:11:28 +0500

    Initial Commit

---
+++ Shared/P176023App.swift
@@ -0,0 +1,17 @@
+//
+//  P176023App.swift
+//  Shared
+//
+//  Created by M Jawad on 22/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct P176023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
